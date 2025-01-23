a793e3e48f9623a8cf11596675845a2ec5dc4760
Yonjay <57708498+Mt-Youya@users.noreply.github.com>
2025-01-23 15:45:35 +0800

    Update teacher_service.rs

--- webservice/src/bin/teacher_service.rs
+++ webservice/src/bin/teacher_service.rs
@@ -1,5 +1,11 @@
+#[path = "../db_access.rs"]
+mod db_access;
+
 #[path = "../handlers.rs"]
 mod handlers;
+
+#[path = "../errors.rs"]
+mod errors;
 
 #[path = "../routers.rs"]
 mod routers;
@@ -10,17 +16,26 @@ mod state;
 #[path = "../models.rs"]
 mod models;
 
+use std::env;
 use actix_web::{web, App, HttpServer};
 use routers::*;
 use state::AppState;
 use std::sync::Mutex;
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_pool = PgPoolOptions::new().connect(database_url.as_str()).await.unwrap();
+
     let shared_data = web::Data::new(AppState {
         health_check_response: "I'm ok".to_string(),
         visit_count: Mutex::new(0),
-        courses: Mutex::new(vec![]),
+        // courses: Mutex::new(vec![]),
+        db: db_pool,
     });
     let app = move || {
         App::new()
