67c4e5abeea0a8e66117f91fb436807eeb62112b
Yonjay <57708498+Mt-Youya@users.noreply.github.com>
2025-01-23 15:44:22 +0800

    Update handler.rs

--- webservice/src/handler.rs
+++ webservice/src/handler.rs
@@ -1,7 +1,8 @@
+use crate::errors::ServiceError;
 use crate::models::Course;
 use crate::state::AppState;
 use actix_web::{web, HttpResponse};
-use chrono::Utc;
+use super::db_access::*;
 
 pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
     let health_check_response = &app_state.health_check_response;
@@ -14,112 +15,92 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
 pub async fn new_course(
     new_course: web::Json<Course>,
     app_state: web::Data<AppState>,
-) -> HttpResponse {
-    let course_count = app_state
-        .courses
-        .lock()
-        .unwrap()
-        .clone()
-        .into_iter()
-        .filter(|course| course.teacher_id == new_course.teacher_id)
-        .collect::<Vec<Course>>()
-        .len();
-
-    let new_course = Course {
-        teacher_id: new_course.teacher_id,
-        id: Some(course_count + 1),
-        name: new_course.name.clone(),
-        time: Some(Utc::now().naive_utc()),
-    };
-    app_state.courses.lock().unwrap().push(new_course);
-    HttpResponse::Ok().json("Course added!")
+) -> Result<HttpResponse,ServiceError> {
+   post_new_course_db(&app_state.db, &new_course.into_inner()).await.map(|course|HttpResponse::Ok().json(course))
 }
 
 pub async fn get_courses_for_teacher(
     app_state: web::Data<AppState>,
     params: web::Path<usize>,
-) -> HttpResponse {
-    let teacher_id: usize = *params;
-    let filtered_courses = app_state
-        .courses
-        .lock()
-        .unwrap()
-        .clone()
-        .into_iter()
-        .filter(|course| course.teacher_id == teacher_id)
-        .collect::<Vec<Course>>();
-    if filtered_courses.len() > 0 {
-        HttpResponse::Ok().json(filtered_courses)
-    } else {
-        HttpResponse::Ok().json("Course not found!".to_string())
-    }
+) -> Result<HttpResponse, ServiceError> {
+    let teacher_id = i32::try_from(*params).expect("Failed to convert usize to i32");
+    get_courses_for_teacher_db(&app_state.db, teacher_id)
+        .await
+        .map(|courses| HttpResponse::Ok().json(courses))
 }
 
 pub async fn get_course_detail(
     app_state: web::Data<AppState>,
     params: web::Path<(usize, usize)>,
-) -> HttpResponse {
-    let (teacher_id, course_id) = *params;
-    let selected_course = app_state
-        .courses
-        .lock()
-        .unwrap()
-        .clone()
-        .into_iter()
-        .find(|x| x.teacher_id == teacher_id && x.id == Some(course_id))
-        .ok_or("Course details not found");
-    if let Ok(course) = selected_course {
-        HttpResponse::Ok().json(course)
-    } else {
-        HttpResponse::Ok().json("Course details not found!".to_string())
-    }
+) -> Result<HttpResponse, ServiceError> {
+    let teacher_id = i32::try_from(params.0).unwrap();
+    let course_id = i32::try_from(params.1).unwrap();
+ get_course_details_db(&app_state.db, teacher_id, course_id).await.map(|course| HttpResponse::Ok().json(course))
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use actix_web::http::StatusCode;
+    use dotenv::dotenv;
+    use sqlx::postgres::PgPoolOptions;
+    use std::env;
     use std::sync::Mutex;
 
     #[actix_rt::test]
     async fn post_course_test() {
-        let course = web::Json(Course {
-            teacher_id: 1,
-            name: "Test course".into(),
-            id: None,
-            time: None,
-        });
+        dotenv().ok();
+
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
+
         let app_state: web::Data<AppState> = web::Data::new(AppState {
             health_check_response: "".to_string(),
             visit_count: Mutex::new(0),
-            courses: Mutex::new(vec![]),
+            db: db_pool,
         });
-
-        let resp = new_course(course, app_state).await;
+        let course = web::Json(Course {
+            teacher_id: 1,
+            name: "Test course".into(),
+            id: Some(24),
+            time: None,
+        });
+        let resp = new_course(course, app_state).await.unwrap();
         assert_eq!(resp.status(), StatusCode::OK);
     }
 
     #[actix_rt::test]
     async fn get_all_course_success() {
+        dotenv().ok();
+
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
+
         let app_state: web::Data<AppState> = web::Data::new(AppState {
             health_check_response: "".to_string(),
             visit_count: Mutex::new(0),
-            courses: Mutex::new(vec![]),
+            db: db_pool,
         });
         let teacher_id: web::Path<usize> = web::Path::from(1);
-        let resp = get_courses_for_teacher(app_state, teacher_id).await;
+        let resp = get_courses_for_teacher(app_state, teacher_id).await.unwrap() ;
         assert_eq!(resp.status(), StatusCode::OK);
     }
 
     #[actix_rt::test]
     async fn get_one_course_success() {
+        dotenv().ok();
+
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env");
+        let db_pool = PgPoolOptions::new().connect(&db_url).await.unwrap();
+
         let app_state: web::Data<AppState> = web::Data::new(AppState {
             health_check_response: "".to_string(),
             visit_count: Mutex::new(0),
-            courses: Mutex::new(vec![]),
+            db: db_pool,
         });
         let params: web::Path<(usize, usize)> = web::Path::from((1, 1));
-        let resp = get_course_detail(app_state, params).await;
+        let resp = get_course_detail(app_state, params).await.unwrap() ;
         assert_eq!(resp.status(), StatusCode::OK);
     }
 }
